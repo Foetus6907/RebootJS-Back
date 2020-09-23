e5d197b7fef4453f54cafe2e63c588feab7fd204
mghoubali <moha.g@hotmail.fr>
2020-09-23 14:42:42 +0200

    refacto position profilRoutes.ts

--- src/routes/profilRoutes.ts
+++ src/routes/profilRoutes.ts
@@ -5,6 +5,18 @@ import {getAllProfiles, getProfile, updateProfil} from "../controllers/profils";
 
 const router = Router();
 
+// Create
+router.post('/', (req: Request, res: Response) => {
+  const {email, firstname, lastname, password} = req.body;
+
+  const newProfile = new Profil({email: email, firstname: firstname, lastname: lastname});
+  newProfile.setPassword(password);
+  newProfile.save();
+
+  res.send('Utilisateur créé');
+});
+
+// Read
 router.get("/me", authenticationRequired, (request: Request, response: Response) => {
   if (!request.user) {
     return response.status(401).send()
@@ -12,7 +24,36 @@ router.get("/me", authenticationRequired, (request: Request, response: Response)
   return response.json((request.user as IProfil).getSafeProfil());
 });
 
-router.patch("/", authenticationRequired, (req, res) => {
+router.get('/:profileId', authenticationRequired, (req: Request, res: Response) => {
+  const profileId = req.params['profileId'];
+
+  getProfile(profileId)
+      .then(profile => {
+        if (profile === null) {
+          return res.status(404).send("Profile not found");
+        }
+        return res.send(profile.getSafeProfil());
+      }).catch(error => {
+        console.error(error);
+        return res.status(500).send()
+      }
+  )
+});
+
+router.get('/', authenticationRequired, (req: Request, res: Response) => {
+  getAllProfiles()
+      .then(profiles => profiles.map(profile => profile.getSafeProfil()))
+      .then(safeProfiles => {
+        return res.status(200).send(safeProfiles);
+      })
+      .catch(error => {
+        console.error(error);
+        return res.status(500).send();
+      })
+});
+
+// Update
+router.patch("/", authenticationRequired, (req: Request, res: Response) => {
   if (!req.user) {
     return res.status(401).send();
   }
@@ -29,7 +70,8 @@ router.patch("/", authenticationRequired, (req, res) => {
     });
 })
 
-router.delete("/", authenticationRequired, (req, res) => {
+// Delete
+router.delete("/", authenticationRequired, (req: Request, res: Response) => {
   if (!req.user) {
     return res.status(401).send("Profil not found")
   }
@@ -41,42 +83,13 @@ router.delete("/", authenticationRequired, (req, res) => {
     });
 });
 
-router.post('/', (req: Request, res: Response) => {
-  const {email, firstname, lastname, password} = req.body;
+router.post('conversation-seen/:conversationId', authenticationRequired, async (req, res) => {
+    const user = req.user as IProfil;
+    const conversationId = req.params['conversationId'];
 
-  const newProfile = new Profil({email: email, firstname: firstname, lastname: lastname});
-  newProfile.setPassword(password);
-  newProfile.save();
-
-  res.send('Utilisateur créé');
-});
-
-router.get('/:profileId', authenticationRequired, (req: Request, res: Response) => {
-  const profileId = req.params['profileId'];
-
-  getProfile(profileId)
-    .then(profile => {
-      if (profile === null) {
-        return res.status(404).send("Profile not found");
-      }
-      return res.send(profile.getSafeProfil());
-    }).catch(error => {
-      console.error(error);
-      return res.status(500).send()
-    }
-  )
-});
-
-router.get('/', (req: Request, res: Response) => {
-  getAllProfiles()
-    .then(profiles => profiles.map(profile => profile.getSafeProfil()))
-    .then(safeProfiles => {
-      return res.status(200).send(safeProfiles);
-    })
-    .catch(error => {
-      console.error(error);
-      return res.status(500).send();
-    })
-});
+    user.updateSeen(conversationId, new Date().toISOString());
+    const savedUser = await user.save();
+    return res.status(200).send(savedUser)
+})
 
 export default router;
